8b27f6ed94cedaccf127c4320ac41c59026d3e83
farhadmpr <farhadmp@gmail.com>
2020-07-25 21:15:32 +0430

    Add Admin Area and Layout

--- AspStore/Endpoint.Site/Startup.cs
+++ AspStore/Endpoint.Site/Startup.cs
@@ -51,6 +51,12 @@ namespace Endpoint.Site
                 endpoints.MapControllerRoute(
                     name: "default",
                     pattern: "{controller=Home}/{action=Index}/{id?}");
+
+                endpoints.MapControllerRoute(
+                    name: "areas",
+                    pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}"
+                );
+
             });
         }
     }
