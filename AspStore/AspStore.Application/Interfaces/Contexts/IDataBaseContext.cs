e07432abca5b741539c9c22c7732cce07b6e2085
farhadmpr <farhadmp@gmail.com>
2020-07-26 21:10:00 +0430

    Add IDataBaseContext

---
+++ AspStore/AspStore.Application/Interfaces/Contexts/IDataBaseContext.cs
@@ -0,0 +1,24 @@
+﻿using AspStore.Domain.Entities.Users;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AspStore.Application.Interfaces.Contexts
+{
+    public interface IDataBaseContext
+    {
+        DbSet<User> Users { get; set; }
+        DbSet<Role> Roles { get; set; }
+        DbSet<UserInRole> UserInRoles { get; set; }
+
+
+        int SaveChanges(bool acceptAllChangesOnSuccess);
+        int SaveChanges();
+        Task<int> SaveChangesAsync(bool acceptAllChangesOnSuccess, CancellationToken cancellationToken = default);
+        Task<int> SaveChangesAsync(CancellationToken cancellationToken = default);
+
+    }
+}
